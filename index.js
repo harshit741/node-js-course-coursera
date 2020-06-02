9b963f8923aa66e7c7fc42c871dbfdaea1ba6b77
harshit741 <luckyharshit741@gmail.com>
2020-06-03 00:10:22 +0530

    express simple REST

--- index.js
+++ index.js
@@ -1,18 +1,60 @@
 const express = require('express');         //requiring Express module
 const http = require('http');
-const morgan = require('morgan');
+const morgan = require('morgan');           // morgan is used of to log request, errors, etc in console
+const parser = require('body-parser');      //requiring body parser module to parse request body
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();      //defining a variable to use express module
 app.use(morgan('dev'));
+app.use(parser.json());
+
+app.all('/dishes', (request, response, next) => {
+    response.statusCode = 200;
+    response.setHeader('Content-Type','text/plain');
+    next();                                             //next() passes the request further to be used
+});
+
+app.get('/dishes', (request, response, next) =>{
+    response.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes', (request, response, next) => {
+    response.end('Will add the dish: ' + request.body.name + ' with details ' + request.body.description);
+});
+
+app.put('/dishes', (request, response, next) => {
+    response.statusCode = 403;
+    response.end('PUT operation not supported!')
+});
+
+app.delete('/dishes',(request, response, next) => {
+    response.end('Deleting all dishes!');
+});
+
+app.get('/dishes/:dishId', (request, response, next) =>{
+    response.end('Will send details of the dish: ' + request.params.dishId + ' to you!');
+});
+
+app.post('/dishes/:dishId', (request, response, next) => {
+    response.statusCode = 403;
+    response.end('POST operation not supported on /dished' + request.params.dishId)});
+
+app.put('/dishes/:dishId', (request, response, next) => {
+    response.write('Updating the dish ' + request.params.dishId + '\n ');
+    response.end('Will update the dish' + request.body.name + ' with details ' + request.body.description);
+});
+
+app.delete('/dishes/:dishId',(request, response, next) => {
+    response.end('Deleting dish!' + request.params.dishId);
+});
 
 app.use(express.static(__dirname + '/public'));
 
 app.use((request, response, next) => {
     response.statusCode = 200;
-    response.setHeader('Content-Type' , 'text/html');
+    response.setHeader('Content-Type','text/html');
     response.end('<html><body><h1>Express server running.</h1></body></html>')
 });
 
