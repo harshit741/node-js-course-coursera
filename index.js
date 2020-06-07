785fc854989e1a1c7b8aff1be6042f417e3c9783
harshit741 <luckyharshit741@gmail.com>
2020-06-07 09:36:25 +0530

    callback hell and Promises

--- index.js
+++ index.js
@@ -5,39 +5,41 @@ const url = 'mongodb://localhost:27017/';               //url of db with port
 const dbname = 'newDb';
 const dboper = require('./operations');                 //requiring operation module
 
-MongoClient.connect(url, (err, client) => {             //creating connection with server
+MongoClient.connect(url, { useUnifiedTopology: true }).then((client) => {             //creating connection with server
 
-    assert.equal(err, null);                            //checking for errors
     console.log('Connected successfully!');
-
     const db = client.db(dbname);                       //creating connection with db
 
     //Nested callbacks of operations imported from operation module
     //passed with required parameters to do various operation
-    dboper.insertDocs(db, {"name" : "Indian Pizza" , "description" : "Cooked and smoked with spices"},      //Inserting
-     "dishes", (result) => { 
+    dboper.insertDocs(db, { "name": "Indian Pizza", "description": "Cooked and smoked with spices" },      //Inserting
+        "dishes")
+    .then((result) => {
         console.log('Insert document\n', result.ops);
+        return dboper.findDocs(db, "dishes")                                //Finding
+    })
+    .then((docs) => {                                                     
+        console.log('Found docs:\n', docs);
+        return dboper.updateDocs(db, { "name": "Indian Pizza" },            //Updating
+        { "description": "Cooked and baked with Indian spices" }, "dishes")
+    })
+    .then((result) => {                                                  
+        console.log('Updated document ', result.result);
+        return dboper.findDocs(db, "dishes")                                //Finding
+    })
+    .then((docs) => {
+        console.log('Found update documents :\n', docs);
+        return dboper.removeDocs(db, { "name": "Indian Pizza" }, "dishes")      //Removing doc
+    })
+    .then((result) => {                                                 
+        console.log('Removed document\n', result.result);
+        return db.dropCollection("dishes")                                      //Droping collection
+    })
+    .then((result) => {                                                
+        console.log('Dropped collection: \n', result);
 
-        dboper.findDocs(db, "dishes", (docs) => {                                                           //Finding
-            console.log('Found docs:\n' + docs);
-
-            dboper.updateDocs(db, {"name" : "Indian Pizza"}, {"description" : "Cooked and baked with Indian spices"},
-            "dishes", (result) => {                                                                         //Updating
-                console.log('Updated document ' , result.result);
-
-                dboper.findDocs(db, "dishes", (docs) => {
-                    console.log('Found update documents :\n' , docs);
-
-                    dboper.removeDocs(db, {"name" : "Indian Pizza"}, "dishes", (result) => {                //Removing doc
-                        console.log('Removed document\n' , result.result);
-
-                        db.dropCollection("dishes", (result) => {                                           //Droping collection
-                            console.log('Dropped collection: \n', result);
-                            client.close();                                                                 //closing collection
-                        })
-                    });
-                });
-            });
-        });
-    });
-});
+        client.close();                                                //closing collection
+    })
+    .catch((err) => console.log(err));
+})
+.catch((err) => console.log(err));
--- operations.js
+++ operations.js
@@ -2,35 +2,20 @@ const assert = require('assert');
 
 exports.insertDocs = (db, doc, collection, callback) => {               //exporting the function
     const coll = db.collection(collection);                             //dreating collection variable
-    coll.insertOne(doc, (err, result) => {                              //insert function with a callback func.
-        assert.equal(err, null);
-        console.log(`Inserted ${result.result.n} into ${collection}`);
-        callback(result);                                               
-    });
+    return coll.insertOne(doc);                                         //removing callback functions 
 };
 
 exports.findDocs = (db, collection, callback) => {
     const coll = db.collection(collection);
-    coll.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
-        callback(docs);
-    });
+    return coll.find({}).toArray();
 };
 
 exports.removeDocs = (db, doc, collection, callback) => {
     const coll = db.collection(collection);
-    coll.deleteOne(doc, (err, result) => {
-        assert.equal(err, null);
-        console.log('Removed the document ', doc);
-        callback(result);
-    });
+    return coll.deleteOne(doc);
 };
 
 exports.updateDocs = (db, doc, update, collection, callback) => {
     const coll = db.collection(collection);
-    coll.updateOne(doc, {$set: update}, null, (err, result) => {
-        assert.equal(err, null);
-        console.log("Updated the document with ", update    );
-        callback(result);
-    });
+    return coll.updateOne(doc, {$set: update}, null);
 };
