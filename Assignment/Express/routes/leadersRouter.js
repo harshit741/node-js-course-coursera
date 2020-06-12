763575438da49e76174309c6061b84fd61216a12
harshit741 <luckyharshit741@gmail.com>
2020-06-12 07:59:41 +0530

    Assignment week 2

--- Assignment/Express/routes/leadersRouter.js
+++ Assignment/Express/routes/leadersRouter.js
@@ -1,47 +1,63 @@
 const express = require('express');
 const parser = require('body-parser');
 const leadersRouter = express.Router();
-
+const Leaders = require('../models/leaders');
 leadersRouter.use(parser.json());
 
 leadersRouter.route('/')
-.all((request, response, next) => {
-    response.statusCode = 200;
-    response.setHeader('Content-Type','text/plain');
-    next();
-})
-.get((request, response, next) => {
-    response.end('Will send all the leaders to you!');
-})
-.post((request, response, next) => {
-    response.end('Will add the leaders: ' + request.body.name + ' with details ' + request.body.description);
-})
-.put((request, response, next) => {
-    response.statusCode = 403;
-    response.end('PUT operation not supported!')
-})
-.delete((request, response, next) => {
-    response.end('Deleting all leaders!');
-});
+    .get((request, response, next) => {
+        Leaders.find({}).then((leaders) => {
+            response.statusCode = 200;
+            response.setHeader('Content-Type','application/json');
+            response.json(leaders);
+        }, err => next(err)).catch(err => next(err));
+    })
+    .post((request, response, next) => {
+        Leaders.create(request.body).then((leader) => {
+            response.statusCode = 200;
+            response.setHeader('Content-Type','application/json');
+            response.json(leader);
+        }, err => next(err)).catch(err => next(err));
+    })
+    .put((request, response, next) => {
+        response.statusCode = 403;
+        response.end('PUT operation not supported on /leaders')
+    })
+    .delete((request, response, next) => {
+        Leaders.remove({}).then((result) => {
+            response.statusCode = 200;
+            response.setHeader('Content-Type','application/json');
+            response.json(result);
+        }, err => next(err)).catch(err => next(err));
+    });
 
 leadersRouter.route('/:leaderId')
-.all((request, response, next) => {
-    response.statusCode = 200;
-    response.setHeader('Content-Type','text/plain');
-    next(); 
-})
-.get((request, response, next) =>{
-    response.end('Will send details of the leader: ' + request.params.leaderId + ' to you!');
-})
-.post((request, response, next) => {
-    response.statusCode = 403;
-    response.end('POST operation not supported on /leaders/leaderId ' + request.params.leaderId)})
-.put((request, response, next) => {
-    response.write('Updating the leader ' + request.params.leaderId + '\n ');
-    response.end('Will update the leader ' + request.body.name + ' with details ' + request.body.description);
-})
-.delete((request, response, next) => {
-    response.end('Deleting leader ' + request.params.leaderId);
-});
+    .get((request, response, next) => {
+        Leaders.findById(request.params.leaderId).then((leader) => {
+            response.statusCode = 200;
+            response.setHeader('Content-Type','application/json');
+            response.json(leader);
+        }, err => next(err)).catch(err => next(err));
+    })
+    .post((request, response, next) => {
+        response.statusCode = 403;
+        response.end('POST operation not supported on /leaders/leaderId ' + request.params.leaderId)
+    })
+    .put((request, response, next) => {
+        Leaders.findByIdAndUpdate(request.params.leaderId,
+            {$set: request.body},
+            {new: true}).then((leader) => {
+                response.statusCode = 200;
+                response.setHeader('Content-Type','application/json');
+                response.json(leader);
+            }, err => next(err)).catch(err => next(err));
+    })
+    .delete((request, response, next) => {
+        Leaders.findByIdAndRemove(request.params.leaderId).then((leader) => {
+            response.statusCode = 200;
+            response.setHeader('Content-Type','application/json');
+            response.json(leader);
+        }, err => next(err)).catch(err => next(err));
+    });
 
 module.exports = leadersRouter;
--- Assignment/Express/routes/promoRouter.js
+++ Assignment/Express/routes/promoRouter.js
@@ -1,47 +1,63 @@
 const express = require('express');
 const parser = require('body-parser');
 const promoRouter = express.Router();
-
+const Promotions = require('../models/promotions');
 promoRouter.use(parser.json());
 
 promoRouter.route('/')
-.all((request, response, next) => {
-    response.statusCode = 200;
-    response.setHeader('Content-Type','text/plain');
-    next();
-})
-.get((request, response, next) => {
-    response.end('Will send all the promotions to you!');
-})
-.post((request, response, next) => {
-    response.end('Will add the promotion: ' + request.body.name + ' with details ' + request.body.description);
-})
-.put((request, response, next) => {
-    response.statusCode = 403;
-    response.end('PUT operation not supported!')
-})
-.delete((request, response, next) => {
-    response.end('Deleting all promotions!');
-});
+    .get((request, response, next) => {
+        Promotions.find({}).then((promotions) => {
+            response.statusCode = 200;
+            response.setHeader('Content-Type', 'application/json');
+            response.json(promotions);
+        }, (err) => next(err)).catch((err) => next(err));
+    })
+    .post((request, response, next) => {
+        Promotions.create(request.body).then((promotion) => {
+            response.statusCode = 200;
+            response.setHeader('Content-Type', 'application/json');
+            response.json(promotion);
+        }, (err) => next(err)).catch((err) => next(err));
+    })
+    .put((request, response, next) => {
+        response.statusCode = 403;
+        response.end('PUT operation not supported on /promtions')
+    })
+    .delete((request, response, next) => {
+        Promotions.remove({}).then((result) => {
+            response.statusCode = 200;
+            response.setHeader('Content-Type', 'application/json');
+            response.json(result);
+        }, (err) => next(err)).catch((err) => next(err));
+    });
 
 promoRouter.route('/:promoId')
-.all((request, response, next) => {
-    response.statusCode = 200;
-    response.setHeader('Content-Type','text/plain');
-    next(); 
-})
-.get((request, response, next) =>{
-    response.end('Will send details of the promotions: ' + request.params.promoId + ' to you!');
-})
-.post((request, response, next) => {
-    response.statusCode = 403;
-    response.end('POST operation not supported on /promotions ' + request.params.promoId)})
-.put((request, response, next) => {
-    response.write('Updating the promotions ' + request.params.promoId + '\n ');
-    response.end('Will update the promotions ' + request.body.name + ' with details ' + request.body.description);
-})
-.delete((request, response, next) => {
-    response.end('Deleting promotions!' + request.params.promoId);
-});
+    .get((request, response, next) => {
+        Promotions.findById(request.params.promoId).then((promotion) => {
+            response.statusCode = 200;
+            response.setHeader('Content-Type', 'application/json');
+            response.json(promotion);
+        }, err => next(err)).catch(err => next(err));
+    })
+    .post((request, response, next) => {
+        response.statusCode = 403;
+        response.end('POST operation not supported on /promotions ' + request.params.promoId);
+    })
+    .put((request, response, next) => {
+        Promotions.findByIdAndUpdate(request.params.promoId,
+            { $set: request.body },
+            { new: true }).then((promotion) => {
+                response.statusCode = 200;
+                response.setHeader('Content-Type','application/json');
+                response.json(promotion);
+            }, err => next(err)).catch(err => next(err));
+    })
+    .delete((request, response, next) => {
+        Promotions.findByIdAndRemove(request.params.promoId).then((promtion) => {
+            response.statusCode = 200;
+            response.setHeader('Content-Type','application/json');
+            response.json(promtion);
+        }, err => next(err)).catch(err => next(err));
+    });
 
 module.exports = promoRouter;
